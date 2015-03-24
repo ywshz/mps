aca4faf436f9f7b3c2a72d051e21ba2af332e46b
ywshz <ywszjut@qq.com>
2015-03-24 15:25:50 +0800

    add jetty to pom, begin to code for web ui

---
+++ src/main/java/com/mopote/mps/WebUiBootstrap.java
@@ -0,0 +1,12 @@
+package com.mopote.mps;
+
+/**
+ * Created by ywshz@vip.qq.com on 2015/3/23.
+ */
+public class WebUiBootstrap {
+    public static void main(String[] args) {
+
+        //jetty
+
+    }
+}
